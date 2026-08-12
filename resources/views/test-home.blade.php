1abd6555f4fb1ceca7c6f06a515e3deea57f7457
Maria ebrahem <mariaebrahem92@gmail.com>
2026-08-12 13:49:50 +0300

    Remove unused test-home view file

--- resources/views/test-home.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.user')
-
-@section('title', 'الصفحة الرئيسية')
-
-@section('content')
-    <h1 class="text-center">أهلاً بيك في نظام الحجوزات</h1>
-    <p class="text-center">لو شايف الصفحة دي متنسقة، يبقى الـ Layout شغال تمام</p>
-@endsection
